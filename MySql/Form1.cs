838499aee0d604f5315956cf66af04d6ad73a8cb
Bartoszdev <bartosz.rozinski@gmail.com>
2017-04-11 15:29:10 +0200

    init

---
+++ MySql/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace MySql
+{
+    public partial class Form1 : Form
+    {
+        public MySqlConnection conn;
+        private string server;
+        private string database;
+        private string Login;
+        private string Email;
+        private string password;
+        private string passwordAgain;
+        private string Miejscowosc;
+        private string Karnet;
+
+
+        public Form1()
+        {
+            server = "localhost";
+            database = "rejestracja";
+            Login = "root";
+            password = "";
+
+            string connString;
+            connString = $"SERVER={server};DATABASE={database};Login={Login};Email={Email};Narodowosc={Miejscowosc};Haslo={password};Powtorz={passwordAgain};Karnet={Karnet};";
+
+            conn = new MySqlConnection(connString);
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            PanelRejestracji PanelRejestracji = new PanelRejestracji();
+            PanelRejestracji.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Login = textBox1.Text;
+
+
+            string haslo = textBox2.Text;
+
+
+            if (IsLogin(textBox1.Text, textBox2.Text));
+            {
+                MessageBox.Show($"Witam {Login} !");
+               
+            
+            { 
+            else
+            {
+                MessageBox.Show($"{Login} nie istnieje lub hasło jest nieprawidłowe !");
+            }
+        }
+        private bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            }
+            {
+
+
+                switch (ex.Number)
+
+
+
+
+                    case 0:
+                    MessageBox.Show("Połączenie z serwerem nie udane");
+                    break;
+                case 1045:
+                    MessageBox.Show("Nazwa użytkownika bądź hasło jest niepoprawne");
+                    break;
+                }
+                return false;
+            }
+        }
+
+        public bool IsLogin(string login, string haslo)
+        {
+            string query = $"SELECT * FROM rejestracja WHERE Login='{login}' AND password='{haslo}';";
+
+            try
+
+            {
+                if (OpenConnection())
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        reader.Close();
+                        conn.Close();
+                        return false;
+
+                    }
+                    else
+                    {
+                        conn.Close();
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                conn.Close();
+                return false;
+            }
+
+        }
+
+
+    }
+}
---
+++ MySql/PanelRejestracji.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace MySql
+{
+    public partial class PanelRejestracji : Form1
+    {
+        public PanelRejestracji()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 Logowanie = new Form1();
+            Logowanie.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Login = textBox1.Text;
+            string email = textBox2.Text;
+            string narodowosci = textBox3.Text;
+            string haslo = textBox4.Text;
+            string powtorzhaslo = textBox5.Text;
+            string karnet = comboBox1.Text;
+            if (Register(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, comboBox1.Text)) ;
+            {
+                MessageBox.Show($"Użytkownik {Login} został zarejestrowany!");
+            }
+            
+            else{
+                MessageBox.Show($"Użytkownik{Login} nie został zarejestrowany!");
+            }
+        }
+
+        public bool Register(string Login, string email, string haslo, string powtorzhaslo, string narodowosci, string karnecik)
+        {
+            string query = $"INSERT INTO rejestracja (Login, email, narodowosc, haslo, powtorzhaslo, karnecik) VALUES ('{Login}','{email}','{narodowosci},'{haslo}','{powtorzhaslo}','{karnecik}');";
+
+            try
+            {
+                if (OpenConnection())
+                {
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        conn.Close();
+                        return false;
+                    }
+                }
+                else
+                {
+                    conn.Close();
+                    return false;
+                }
+
+            }
+
+
+            }
+    }
+}
+
+
+
+            
+            
+ 
+         
+
+
+
+        
+                 
+           
+      
+        
+    
